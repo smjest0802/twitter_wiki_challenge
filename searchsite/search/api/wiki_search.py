724063a963e1cdbc89a209cbdadec11e7b84d210
Shawn Meginley <smjest0802@gmail.com>
2015-09-28 11:33:21 -0400

    - Updated query_search.py to access Wiki API and parse results into a list of class QueryResult. - Updated views.py to call queryWiki from wiki_search.py and pass the results into page render. - Updated index.html to display Wiki search results and added a place holder for twitter results. - Added an index.css to handle page formating.
    Next steps:
    - Add some unit tests.
    - Handle no access to Wiki.

--- searchsite/search/api/wiki_search.py
+++ searchsite/search/api/wiki_search.py
@@ -1,23 +1,31 @@
-import requests
+import requests, json
 
-class WikiSearch:
+class QueryResult():
 
-    #def __init__(self, data):
-    field1 = "Test 1"
-    field2 = "Test 2"
+    def __init__(self, result):
+        self.ns = result.get('ns')
+        self.title = result.get('title')
+        self.snippet = result.get('snippet')
+        self.size = result.get('size')
+        self.wordcount = result.get('wordcount')
+        self.timestamp = result.get('timestamp')
 
 
-def queryWiki():
+
+def queryWiki(searchTerm):
 
     # sample url: https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Albert%20Einstein&utf8=
-
     r = requests.get('https://en.wikipedia.org/w/api.php', params = {'action': 'query',
                                                             'list': 'search',
-                                                            'srsearch': 'Albert Einstein',  # Need to url encode this field
+                                                            'srsearch': searchTerm,  
                                                             'format': 'json',
-                                                            #'utf8':''
                                                            })
 
-    print r.text
+    wikiJson = json.loads(r.text)
 
-    return WikiSearch()
+    wikiResults = []
+
+    for result in wikiJson.get('query', {}).get('search', {}):
+        wikiResults.append(QueryResult(result))
+
+    return wikiResults
--- searchsite/search/views.py
+++ searchsite/search/views.py
@@ -8,14 +8,12 @@ def IndexView(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        print "Here 2"
         form = SearchForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            print "Here 3"
 
             search_text = form.cleaned_data['search_text']
 
@@ -25,12 +23,11 @@ def IndexView(request):
             #from api.wiki_search import queryWiki
             from .api.wiki_search import queryWiki
 
-            query_results = queryWiki()
+            query_results = queryWiki(search_text)
 
             return render(request, 'search/index.html', {'form':form, 'search_text': search_text, 'query_results': query_results})
     # if a GET (or any other method) we'll create a blank form
     else:
-        print "Here 1"
         form = SearchForm()
 
 
